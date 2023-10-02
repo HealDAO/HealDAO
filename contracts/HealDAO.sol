4463cde7abdd7f07dc750cbbcd6e98a1a371e048
Reza Fatahi <aug2uag@gmail.com>
2023-10-02 08:32:49 -0700

    permissioned HealDAO

--- contracts/HealDAO.sol
+++ contracts/HealDAO.sol
@@ -1,7 +1,69 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract HealthRecordsDAO {
+contract HealDAO {
+
+    address public owner;
+    mapping(address => bool) public accessor;
+
+    enum DataLimitType {
+        ALLERGIES,
+        BLOODWORKS,
+        DIAGNOSES,
+        GLOBALS,
+        MEDICATIONS,
+        PROCEDURES,
+        VITALS
+    }
+
+    uint16 public MAX_ALLERGIES_LENGTH = 100;
+    uint16 public MAX_BLOODWORKS_LENGTH = 100;
+    uint16 public MAX_DIAGNOSES_LENGTH = 100;
+    uint16 public MAX_GLOBALS_LENGTH = 100;
+    uint16 public MAX_MEDICATIONS_LENGTH = 100;
+    uint16 public MAX_PROCEDURES_LENGTH = 100;
+    uint16 public MAX_VITALS_LENGTH = 100;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not authorized");
+        _;
+    }
+
+    modifier isAllowed() {
+        require(accessor[msg.sender] || msg.sender == owner, "Not authorized");
+        _;
+    }
+
+    function grantAccess(address _address) public onlyOwner {
+        accessor[_address] = true;
+    }
+
+    function revokeAccess(address _address) public onlyOwner {
+        accessor[_address] = false;
+    }
+
+    // List limits
+    function setListLimit(DataLimitType dataType, uint16 newLength) public onlyOwner {
+        if (dataType == DataLimitType.ALLERGIES) {
+            MAX_ALLERGIES_LENGTH = newLength;
+        } else if (dataType == DataLimitType.BLOODWORKS) {
+            MAX_BLOODWORKS_LENGTH = newLength;
+        } else if (dataType == DataLimitType.DIAGNOSES) {
+            MAX_DIAGNOSES_LENGTH = newLength;
+        } else if (dataType == DataLimitType.GLOBALS) {
+            MAX_GLOBALS_LENGTH = newLength;
+        } else if (dataType == DataLimitType.MEDICATIONS) {
+            MAX_MEDICATIONS_LENGTH = newLength;
+        } else if (dataType == DataLimitType.PROCEDURES) {
+            MAX_PROCEDURES_LENGTH = newLength;
+        } else if (dataType == DataLimitType.VITALS) {
+            MAX_VITALS_LENGTH = newLength;
+        }
+    }
 
     // Subcomponents
     struct Date {
@@ -18,7 +80,7 @@ contract HealthRecordsDAO {
     struct Bloodwork {
         string bloodworkAbbreviation;
         uint16 measure;
-        uint8 multiplier;  // -100 to 100, ±1000 would be new unit
+        uint8 multiplier;
         string unitAbbreviation;
         Date timestamp;
     }
@@ -50,7 +112,7 @@ contract HealthRecordsDAO {
     struct Procedure {
         string procedureAbbreviation;
         uint16 numTimes;
-        string locationList; // Comma-separated locations
+        string locationList;
     }
 
     struct Vital {
@@ -76,55 +138,118 @@ contract HealthRecordsDAO {
     event MedicationAdded(address indexed records, string indexed drugAbbreviation);
     event ProcedureAdded(address indexed records, string procedureType);
     event VitalAdded(address indexed records, string vitalType, Date timestamp);
-    event UpdatedMedications(address indexed records);
     event UpdatedAllergies(address indexed records);
     event UpdatedBloodworks(address indexed records);
     event UpdatedDiagnoses(address indexed records);
     event UpdatedGlobals(address indexed records);
+    event UpdatedMedications(address indexed records);
     event UpdatedProcedures(address indexed records);
     event UpdatedVitals(address indexed records);
 
-    function addAllergy(string memory allergyAbbreviation) public {
+    function addAllergy(string memory allergyAbbreviation) public isAllowed {
         allergies.push(Allergy(allergyAbbreviation));
         emit AllergyAdded(address(this), allergyAbbreviation);
+
+        // Remove the oldest allergy if the length exceeds the max length
+        while (allergies.length > MAX_ALLERGIES_LENGTH) {
+            // Remove the first element
+            for (uint i = 0; i < allergies.length - 1; i++) {
+                allergies[i] = allergies[i + 1];
+            }
+            allergies.pop();
+        }
     }
 
-    function addBloodwork(string memory bloodworkAbbreviation, uint16 measure, uint8 multiplier, string memory unitAbbreviation, uint16 day, uint16 month, uint16 year) public {
+    function addBloodwork(string memory bloodworkAbbreviation, uint16 measure, uint8 multiplier, string memory unitAbbreviation, uint16 day, uint16 month, uint16 year) public isAllowed {
         Date memory timestamp = Date(day, month, year);
         bloodworks.push(Bloodwork(bloodworkAbbreviation, measure, multiplier, unitAbbreviation, timestamp));
         emit BloodworkAdded(address(this), bloodworkAbbreviation, measure, unitAbbreviation, timestamp);
+
+        // Remove the oldest bloodwork if the length exceeds the max length
+        while (bloodworks.length > MAX_BLOODWORKS_LENGTH) {
+            // Remove the first element
+            for (uint i = 0; i < bloodworks.length - 1; i++) {
+                bloodworks[i] = bloodworks[i + 1];
+            }
+            bloodworks.pop();
+        }
     }
 
-    function addDiagnosis(string memory diagnosisAbbreviation, uint16 numTimes, string memory locationList) public {
+    function addDiagnosis(string memory diagnosisAbbreviation, uint16 numTimes, string memory locationList) public isAllowed {
         diagnoses.push(Diagnosis(diagnosisAbbreviation, numTimes, locationList));
         emit DiagnosisAdded(address(this), diagnosisAbbreviation);
+
+        // Remove the oldest diagnosis if the length exceeds the max length
+        while (diagnoses.length > MAX_DIAGNOSES_LENGTH) {
+            // Remove the first element
+            for (uint i = 0; i < diagnoses.length - 1; i++) {
+                diagnoses[i] = diagnoses[i + 1];
+            }
+            diagnoses.pop();
+        }
     }
 
-    function addGlobal(string memory globalAbbreviation, uint16 measure, uint8 multiplier, string memory unitAbbreviation, uint16 day, uint16 month, uint16 year) public {
+    function addGlobal(string memory globalAbbreviation, uint16 measure, uint8 multiplier, string memory unitAbbreviation, uint16 day, uint16 month, uint16 year) public isAllowed {
         Date memory timestamp = Date(day, month, year);
         globals.push(Global(globalAbbreviation, measure, multiplier, unitAbbreviation, timestamp));
         emit GlobalInfoAdded(address(this), globalAbbreviation, timestamp);
+
+        // Remove the oldest global if the length exceeds the max length
+        while (globals.length > MAX_GLOBALS_LENGTH) {
+            // Remove the first element
+            for (uint i = 0; i < globals.length - 1; i++) {
+                globals[i] = globals[i + 1];
+            }
+            globals.pop();
+        }
     }
 
-    function addMedication(string memory drugAbbreviation, uint16 dose, uint8 multiplier, string memory unitAbbreviation, uint16 frequency, uint16 duration, uint16 lastDay, uint16 lastMonth, uint16 lastYear) public {
+    function addMedication(string memory drugAbbreviation, uint16 dose, uint8 multiplier, string memory unitAbbreviation, uint16 frequency, uint16 duration, uint16 lastDay, uint16 lastMonth, uint16 lastYear) public isAllowed {
         Date memory lastDate = Date(lastDay, lastMonth, lastYear);
         medications.push(Medication(drugAbbreviation, dose, multiplier, unitAbbreviation, frequency, duration, lastDate));
         emit MedicationAdded(address(this), drugAbbreviation);
+
+        // Remove the oldest medication if the length exceeds the max length
+        while (medications.length > MAX_MEDICATIONS_LENGTH) {
+            // Remove the first element
+            for (uint i = 0; i < medications.length - 1; i++) {
+                medications[i] = medications[i + 1];
+            }
+            medications.pop();
+        }
     }
 
-    function addProcedure(string memory procedureAbbreviation, uint16 numTimes, string memory locationList) public {
+    function addProcedure(string memory procedureAbbreviation, uint16 numTimes, string memory locationList) public isAllowed {
         procedures.push(Procedure(procedureAbbreviation, numTimes, locationList));
         emit ProcedureAdded(address(this), procedureAbbreviation);
+
+        // Remove the oldest procedure if the length exceeds the max length
+        while (procedures.length > MAX_PROCEDURES_LENGTH) {
+            // Remove the first element
+            for (uint i = 0; i < procedures.length - 1; i++) {
+                procedures[i] = procedures[i + 1];
+            }
+            procedures.pop();
+        }
     }
 
-    function addVital(string memory vitalAbbreviation, uint16 measure, uint8 multiplier, string memory unitAbbreviation, uint16 day, uint16 month, uint16 year) public {
+    function addVital(string memory vitalAbbreviation, uint16 measure, uint8 multiplier, string memory unitAbbreviation, uint16 day, uint16 month, uint16 year) public isAllowed {
         Date memory timestamp = Date(day, month, year);
         vitals.push(Vital(vitalAbbreviation, measure, multiplier, unitAbbreviation, timestamp));
         emit VitalAdded(address(this), vitalAbbreviation, timestamp);
+
+        // Remove the oldest vital if the length exceeds the max length
+        while (vitals.length > MAX_VITALS_LENGTH) {
+            // Remove the first element
+            for (uint i = 0; i < vitals.length - 1; i++) {
+                vitals[i] = vitals[i + 1];
+            }
+            vitals.pop();
+        }
     }
 
     // Individual getters
-    function getAllergies() public view returns (string memory) {
+    function getAllergies() public view isAllowed returns (string memory) {
         string memory result = "";
         for(uint16 i = 0; i < allergies.length; i++) {
             result = string(abi.encodePacked(result, allergies[i].allergyAbbreviation, "; "));
@@ -132,7 +257,7 @@ contract HealthRecordsDAO {
         return result;
     }
 
-    function getBloodworks() public view returns (string memory) {
+    function getBloodworks() public view isAllowed returns (string memory) {
         string memory result = "";
         for(uint16 i = 0; i < bloodworks.length; i++) {
             Bloodwork memory bw = bloodworks[i];
@@ -141,7 +266,7 @@ contract HealthRecordsDAO {
         return result;
     }
 
-    function getDiagnoses() public view returns (string memory) {
+    function getDiagnoses() public view isAllowed returns (string memory) {
         string memory result = "";
         for(uint16 i = 0; i < diagnoses.length; i++) {
             Diagnosis memory diag = diagnoses[i];
@@ -151,7 +276,7 @@ contract HealthRecordsDAO {
         return result;
     }
 
-    function getGlobals() public view returns (string memory) {
+    function getGlobals() public view isAllowed returns (string memory) {
         string memory result = "";
         for(uint16 i = 0; i < globals.length; i++) {
             Global memory glob = globals[i];
@@ -160,7 +285,7 @@ contract HealthRecordsDAO {
         return result;
     }
 
-    function getMedications() public view returns (string memory) {
+    function getMedications() public view isAllowed returns (string memory) {
         string memory result = "";
         for(uint16 i = 0; i < medications.length; i++) {
             Medication memory med = medications[i];
@@ -169,7 +294,7 @@ contract HealthRecordsDAO {
         return result;
     }
 
-    function getProcedures() public view returns (string memory) {
+    function getProcedures() public view isAllowed returns (string memory) {
         string memory result = "";
         for(uint16 i = 0; i < procedures.length; i++) {
             Procedure memory proc = procedures[i];
@@ -179,77 +304,13 @@ contract HealthRecordsDAO {
         return result;
     }
 
-    function getVitals() public view returns (string memory) {
+    function getVitals() public view isAllowed returns (string memory) {
         string memory result = "";
         for(uint16 i = 0; i < vitals.length; i++) {
             Vital memory vit = vitals[i];
             result = string(abi.encodePacked(result, vit.vitalAbbreviation, "-", uint2str(vit.measure), "-", uint2str(vit.multiplier), "-", vit.unitAbbreviation, "-", date2str(vit.timestamp), "; "));
         }
         return result;
-    }
-
-    function updateAllergies(Allergy[] memory newAllergies) public {
-        delete allergies;
-
-        for(uint i = 0; i < newAllergies.length; i++) {
-            allergies.push(newAllergies[i]);
-        }
-        emit UpdatedAllergies(address(this));
-    }
-
-    function updateBloodworks(Bloodwork[] memory newBloodworks) public {
-        delete bloodworks;
-
-        for(uint i = 0; i < newBloodworks.length; i++) {
-            bloodworks.push(newBloodworks[i]);
-        }
-        emit UpdatedBloodworks(address(this));
-    }
-
-    function updateDiagnoses(Diagnosis[] memory newDiagnoses) public {
-        delete diagnoses;
-
-        for(uint i = 0; i < newDiagnoses.length; i++) {
-            diagnoses.push(newDiagnoses[i]);
-        }
-
-        emit UpdatedDiagnoses(address(this));
-    }
-
-    function updateGlobals(Global[] memory newGlobals) public {
-        delete globals;
-
-        for(uint i = 0; i < newGlobals.length; i++) {
-            globals.push(newGlobals[i]);
-        }
-        emit UpdatedGlobals(address(this));
-    }
-
-    function updateMedications(Medication[] memory newMedications) public {
-        delete medications;
-
-        for(uint i = 0; i < newMedications.length; i++) {
-            medications.push(newMedications[i]);
-        }
-        emit UpdatedMedications(address(this));
-    }
-
-    function updateProcedures(Procedure[] memory newProcedures) public {
-        delete procedures;
-
-        for(uint i = 0; i < newProcedures.length; i++) {
-            procedures.push(newProcedures[i]);
-        }
-        emit UpdatedProcedures(address(this));
-    }
-
-    function updateVitals(Vital[] memory newVitals) public {
-        delete vitals;
-
-        for(uint i = 0; i < newVitals.length; i++) {
-            vitals.push(newVitals[i]);
-        }
-        emit UpdatedVitals(address(this));
     }
 
     // Helper functions to convert uint to string and Date to string format
