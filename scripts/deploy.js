c9b42a3d79e09b26240bdddfd818b53f11c80f64
Reza Fatahi <aug2uag@gmail.com>
2023-10-02 10:28:35 -0700

    deployment script

---
+++ scripts/deploy.js
@@ -0,0 +1,21 @@
+const hre = require("hardhat");
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const Contract = await hre.ethers.getContractFactory("HealDAO");
+  const contract = await Contract.deploy();
+
+  await contract.deployed();
+
+  console.log("Contract deployed to:", contract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
